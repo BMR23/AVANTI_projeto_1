78fa90b71615fbc7689637218f02359d720568ae
BMR23 <brenomelo161@hotmail.com>
2025-04-21 01:33:07 -0300

    Starting desktop desing

--- scritps/script.js
+++ scritps/script.js
@@ -1,11 +1,16 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
-              
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
+
+let swiper;
+let margin_card = window.innerWidth < 1024 ? 10 : 17;
+
+function iniciarSwiper() {
+  if (swiper) swiper.destroy(true, true); // Destroi o swiper anterior, se existir
+
+  swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 'auto',
-    spaceBetween: 10,
+    spaceBetween: margin_card,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -17,6 +22,19 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
-    
+  });
 }
-)
+
+// Inicializa o swiper pela primeira vez
+iniciarSwiper();
+
+// Adiciona o listener para redimensionamento
+window.addEventListener('resize', () => {
+  const largura = window.innerWidth;
+  const novo_margin = largura < 1024 ? 10 : 17;
+
+  if (novo_margin !== margin_card) {
+    margin_card = novo_margin;
+    iniciarSwiper(); // Recria o swiper com novo valor de spaceBetween
+  }
+});
